62dade45dfa50a8b5cd3c56096ac227816aa0bcd
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-12-10 12:12:21 +0100

    feat: add separate admin view for listing all logs
    Closes: #27

--- apis_acdhch_django_auditlog/urls.py
+++ apis_acdhch_django_auditlog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apis_acdhch_django_auditlog.views import UserAuditLog
+from apis_acdhch_django_auditlog.views import AuditLog, UserAuditLog
 
 
 urlpatterns = [
-    path("auditlog", UserAuditLog.as_view()),
+    path("profile/auditlog", UserAuditLog.as_view()),
+    path("auditlog", AuditLog.as_view()),
 ]
