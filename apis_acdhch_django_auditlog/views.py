a52c9e9f83854aef972558818aa3f128a4f9e61e
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-12-10 12:24:45 +0100

    feat: add pagination to views

--- apis_acdhch_django_auditlog/views.py
+++ apis_acdhch_django_auditlog/views.py
@@ -4,12 +4,15 @@ from auditlog.models import LogEntry
 
 
 class UserAuditLog(LoginRequiredMixin, ListView):
+    paginate_by = 100
+
     def get_queryset(self, *args, **kwargs):
         return LogEntry.objects.filter(actor=self.request.user)
 
 
 class AuditLog(UserPassesTestMixin, ListView):
     model = LogEntry
+    paginate_by = 100
 
     def test_func(self):
         return self.request.user.is_superuser
