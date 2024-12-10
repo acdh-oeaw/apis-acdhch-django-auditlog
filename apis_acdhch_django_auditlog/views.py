261f167b48e3413c35ce7722989d62a3bca4d435
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-12-10 12:12:21 +0100

    feat: add separate admin view for listing all logs
    Closes: #27

--- apis_acdhch_django_auditlog/views.py
+++ apis_acdhch_django_auditlog/views.py
@@ -1,8 +1,15 @@
 from django.views.generic.list import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from auditlog.models import LogEntry
 
 
 class UserAuditLog(LoginRequiredMixin, ListView):
     def get_queryset(self, *args, **kwargs):
         return LogEntry.objects.filter(actor=self.request.user)
+
+
+class AuditLog(UserPassesTestMixin, ListView):
+    model = LogEntry
+
+    def test_func(self):
+        return self.request.user.is_superuser
